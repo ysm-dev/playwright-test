2c0572e8735a17c60b0e186246fcd10a5a094a70
ysm-dev <i@ysm.dev>
2023-10-25 14:27:12 +0900

    enable cron

--- tests/movie.spec.ts
+++ tests/movie.spec.ts
@@ -1,28 +1,30 @@
-import { test, expect } from '@playwright/test'
-import { chromium } from 'playwright-extra'
-import stealth from 'puppeteer-extra-plugin-stealth'
-import 'dotenv/config'
+import { test, expect } from "@playwright/test";
+import { chromium } from "playwright-extra";
+import stealth from "puppeteer-extra-plugin-stealth";
+import "dotenv/config";
 
-const DO_URL = process.env.DO_URL
+const DO_URL = process.env.DO_URL;
 
-chromium.use(stealth())
+chromium.use(stealth());
 
-test('Movie', async ({}) => {
-  const browser = await chromium.launch()
-  const context = await browser.newContext()
-  const page = await context.newPage()
+test("Movie", async ({}) => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
   await page.goto(`https://www.crunchbase.com/browser-extension`, {
-    waitUntil: 'domcontentloaded',
-  })
+    waitUntil: "domcontentloaded",
+  });
 
-  const cookie = await getCookie(page)
+  const cookie = await getCookie(page);
 
-  await saveCookie(cookie)
-  console.log({ cookie })
+  await saveCookie(cookie);
+  console.log({ cookie });
 
-  await expect(cookie).toBeTruthy()
-})
+  await expect(cookie).toBeTruthy();
+});
 
 const getCookie = async (page: any) => {
   return await page.evaluate(`(() => {
@@ -41,45 +43,45 @@ const getCookie = async (page: any) => {
         },
       )
     })
-  })()`)
-}
+  })()`);
+};
 
 const saveCookie = async (newData: any) => {
   try {
-    const data: object[] = await getData()
+    const data: object[] = await getData();
 
-    await setData([newData, ...data])
+    await setData([newData, ...data]);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 
-  return newData
-}
+  return newData;
+};
 
 export const setDO = async (key: string, value: any) => {
   const res = await fetch(`${DO_URL}/do/${encodeURIComponent(key)}`, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(value),
-  })
+  });
 
   if (res.status !== 200) {
-    throw new Error(`Failed to set DO: ${res.status}`)
+    throw new Error(`Failed to set DO: ${res.status}`);
   }
 
-  return res.json()
-}
+  return res.json();
+};
 
 export async function getDO<T = any>(key: string) {
-  const res = await fetch(`${DO_URL}/do/${encodeURIComponent(key)}`)
+  const res = await fetch(`${DO_URL}/do/${encodeURIComponent(key)}`);
 
   if (res.status > 399) {
-    return null
+    return null;
   }
 
-  return res.json() as Promise<T>
+  return res.json() as Promise<T>;
 }
 
-const getData = () => getDO('ec321f3e-d0ec-496f-abc9-4a214ce6e5fc')
+const getData = () => getDO("ec321f3e-d0ec-496f-abc9-4a214ce6e5fc");
 
 const setData = (data: any) =>
-  setDO('ec321f3e-d0ec-496f-abc9-4a214ce6e5fc', data)
+  setDO("ec321f3e-d0ec-496f-abc9-4a214ce6e5fc", data);
